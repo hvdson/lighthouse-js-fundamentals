e234395c6bc172ab8af267105e0c7ce5ede3d4b5
hvdson <hvdsonmusic@gmail.com>
2017-11-17 03:39:19 +0000

    Adds looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,19 @@
+var chorus = "Let's dance!";
+var repeat = 0;
+
+// while loop
+// while (repeat < 10) {
+//   if (repeat == 5) {
+//     console.log("*change key*");
+//   }
+//   console.log(chorus);
+//   repeat++;
+// }
+
+// for loop
+for (var repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+
+console.log("Until the sun comes up!");
+
