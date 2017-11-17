68a5da66cdf02428e530b61d9f397097297e9837
hvdson <hvdsonmusic@gmail.com>
2017-11-17 04:01:34 +0000

    Printing ingredients exercises

---
+++ ingredients.js
@@ -0,0 +1,33 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+function printIngredientsWhileLoop(items){
+  var i = 0;
+  while (i < items.length - 1) {
+    console.log(items[i]);
+    i++;
+  }
+}
+console.log("While Loop:");
+printIngredientsWhileLoop(ingredients);
+
+// Write a for loop that prints out the contents of ingredients:
+
+function printIngredientsForLoop(items) {
+  for (var i = 0; i < items.length; i++) {
+    console.log(items[i]);
+  }
+}
+console.log("\nFor Loop:");
+printIngredientsForLoop(ingredients);
+
+// Write any loop (while or for) that prints out the contents of the ingredients backwards
+
+function printIngredientsBackwards(items) {
+  for (var i = items.length - 1; i >= 0; i--) {
+    console.log(items[i]);
+  }
+}
+
+console.log("\nBackwards:")
+printIngredientsBackwards(ingredients);
